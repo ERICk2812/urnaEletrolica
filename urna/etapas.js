b17c9359417c9302ca082b553428162633ec213b
ERICK <sampaioerick28@gmail.com>
2022-11-08 19:50:19 -0300

    first commit

---
+++ urna/etapas.js
@@ -0,0 +1,58 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numero: 5,
+    candidato: [
+      {
+        numero: 14152,
+        name: 'CARLOS ALBERTO',
+        partido: 'PSDB',
+        fotos: [
+          { url: 'candidato 1.jpeg', legenda: 'VEREADOR' }
+        ]
+      },
+      {
+        numero: 24525,
+        name: 'JOSE ANTONIO',
+        partido: 'PL',
+        fotos: [
+          { url: 'cadidato 2.jpeg', legenda: 'VEREADOR' }
+        ]
+      }
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numero: 2,
+    candidato: [
+      {
+        numero: 99,
+        name: 'jeferson',
+        patido: 'jps',
+        vice: 'silva',
+        fotos: [
+          { url: 'candidato 3.jpeg', legenda: 'Prefeito' },
+          { url: 'candidato 4.jpeg', Legenda: 'VICE-PREFEITO', Small: true }
+        ]
+      }
+    ]
+  },
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
